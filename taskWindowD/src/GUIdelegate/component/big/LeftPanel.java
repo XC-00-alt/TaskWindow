d140e2a62d72cb6d22e5c129d64ab383083dc8fd
x <cai_x00@163.com>
2025-01-04 13:27:21 +0800

    try str instead of label

--- taskWindowD/src/GUIdelegate/component/big/LeftPanel.java
+++ taskWindowD/src/GUIdelegate/component/big/LeftPanel.java
@@ -8,21 +8,29 @@ import java.awt.*;
 public class LeftPanel extends JPanel{
     int width;
     int height;
-    private CustomizedLabel importantLabel=new CustomizedLabel("IMPORTANT",0);
-    private CustomizedLabel unimportantLabel=new CustomizedLabel("unimportant",0);
+//    private CustomizedLabel importantLabel=new CustomizedLabel("I",0);
+//    private CustomizedLabel unimportantLabel=new CustomizedLabel("unimportant",0);
+    private String importantLabel="IMPORTANT";
+    private String unimportantLabel="unimportant";
+    private double rotationRadius=Math.toRadians(-90);
     public LeftPanel(int width,int height)
     {
+        this.width=width;
+        this.height=height;
         this.setPreferredSize(new Dimension(width,height));
-        add(importantLabel);
-        importantLabel.setBounds(width/2-importantLabel.getWidth()/2,height/3,
-                importantLabel.getWidth(),importantLabel.getHeight());
-//        add(unimportantLabel);
     }
     @Override
     public void paintComponent(Graphics g)
     {
         g.setColor(Color.YELLOW);
         g.fillRect(0,0,getWidth(),getHeight());
-//        super.paintComponent(g);
+        g.setColor(Color.black);
+        Graphics2D g2d=(Graphics2D)g;
+        g2d.rotate(rotationRadius,getWidth()/2,getHeight()/3);
+        g2d.drawString(importantLabel,0,getHeight()/3);
+        // should rotate back first
+        g2d.rotate(-rotationRadius,getWidth()/2,getHeight()/3);
+        g2d.rotate(rotationRadius,getWidth()/2,getHeight()*2/3);
+        g2d.drawString(unimportantLabel,0,getHeight()*2/3);
     }
 }
